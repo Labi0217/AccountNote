b8186540a39c40f9c8c207fc9c94f358717efcb5
Libi0217 <wlsglckals@gmail.com>
2020-07-28 22:23:26 +0900

    Commit

--- app/src/main/java/com/sohee/accountnote/AddPlace.java
+++ app/src/main/java/com/sohee/accountnote/AddPlace.java
@@ -3,35 +3,53 @@ package com.sohee.accountnote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class AddPlace extends AppCompatActivity {
+    private DBHelper notedb;
     TextView text1;
+    Button save1;
+    EditText namee;
+    EditText coste;
+    EditText contente;
+    Spinner spinner;
+    String[] items = {"지역", "서울특별시", "인천광역시","울산광역시","충청북도","전라남도"
+            , "부산광역시", "광주광역시", "경기도", "충청남도", "경상북도", "경상남도", "대구광역시", "대전광역시", "강원도"
+            , "전라북도", "제주특별자치도"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_place);
+        spinner = findViewById(R.id.spinner);
+        text1 = (TextView) findViewById(R.id.textView);
+        namee = (EditText) findViewById(R.id.nameE);
+        coste = (EditText) findViewById(R.id.costE);
+        contente = (EditText) findViewById(R.id.contentE);
+        save1 = (Button) findViewById(R.id.saveB);
 
-        text1 = (TextView)findViewById(R.id.textView);
-    }
+        notedb = new DBHelper(this);
 
-    public void onClick(View v) {
-        if(v == text1) {
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
+        save1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FindPlace.class);
+                startActivity(intent);
+            }
+        });
 
-            DatePickerDialog D = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-                public void onDateSet(DatePicker view, int year1, int month1, int day1) {
-                    text1.setText(year1+"-"+month1+"-"+day1);
-                }
-            }, year, month, day);
-        }
     }
 }
--- app/src/main/java/com/sohee/accountnote/DBHelper.java
+++ app/src/main/java/com/sohee/accountnote/DBHelper.java
@@ -1,4 +1,42 @@
 package com.sohee.accountnote;
 
-public class DBHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Account_Note.db";
+    public static final int DATABASE_VERSION = 1;
+    public static final String NOTE_TITLE = "title";
+    public static final String NOTE_COST ="cost";
+    public static final String NOTE_CONTENT = "content1";
+
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS note");
+        onCreate(db);
+    }
+
+    public boolean insertNote(String title, String cost, String content1) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("title", title);
+        contentValues.put("cost", cost);
+        contentValues.put("content1", content1);
+
+        db.insert("note", null, contentValues);
+        return true;
+    }
 }
